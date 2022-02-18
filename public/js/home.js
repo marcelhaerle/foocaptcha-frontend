36780859efe0289513ccb0286810cd3c0081e557
Marcel Härle <marcel.haerle@sonnvest.de>
2022-02-18 08:39:35 +0100

    Add chars and equation demo section

--- public/js/home.js
+++ public/js/home.js
@@ -1,16 +1,107 @@
 import { CountUp } from "./countUp.min.js";
+import $ from "./jquery.module.js";
 
 window.onload = function() {
-  fetch('/api/stats').then(response => {
-    response.json().then(jsonResponse => {
-      const totalCaptachas = new CountUp('totalCaptchas', jsonResponse['totalCaptchas']);
+  const loadStats = () => {
+    $.get('/api/stats').then(response => {
+      const totalCaptachas = new CountUp('totalCaptchas', response['totalCaptchas']);
       totalCaptachas.start();
 
-      const totalViewed = new CountUp('totalViewed', jsonResponse['totalViewed']);
+      const totalViewed = new CountUp('totalViewed', response['totalViewed']);
       totalViewed.start();
 
-      const totalVerified = new CountUp('totalVerified', jsonResponse['totalVerified']);
+      const totalVerified = new CountUp('totalVerified', response['totalVerified']);
       totalVerified.start();
-    })
-  })
+    });
+  };
+
+  const charsGuess = $('#charsGuess');
+  const imgChars = $('#imgChars');
+  const reloadCharsButton = $('#reloadCharsButton');
+  const verifyCharsButton = $('#verifyCharsButton');
+  const charsVerifyResult = $('#charsVerifyResult');
+
+  const equationGuess = $('#equationGuess');
+  const imgEquation = $('#imgEquation');
+  const reloadEquationButton = $('#reloadEquationButton');
+  const verifyEquationButton = $('#verifyEquationButton');
+  const equationVerifyResult = $('#equationVerifyResult');
+
+  const loadCharsCaptcha = () => {
+    $.get('/api/chars').then(response => {
+      imgChars
+        .attr('src', response.url)
+        .attr('data-cid', response.id);
+      verifyCharsButton.attr('disabled', null);
+      charsGuess.val('');
+      charsVerifyResult.text('');
+      loadStats();
+    });
+  };
+
+  const loadEquationCaptcha = () => {
+    $.get('/api/equation').then(response => {
+      imgEquation
+        .attr('src', response.url)
+        .attr('data-cid', response.id);
+      verifyEquationButton.attr('disabled', null);
+      equationGuess.val('');
+      equationVerifyResult.text('');
+      loadStats();
+    });
+  };
+
+  // Empty guess input field.
+  charsGuess.val('');
+  equationGuess.val('');
+
+  // Init loading captchas.
+  loadCharsCaptcha();
+  loadEquationCaptcha();
+
+  reloadCharsButton.on('click', loadCharsCaptcha);
+
+  verifyCharsButton.on('click', () => {
+    const id = imgChars.attr('data-cid');
+    const guess = charsGuess.val();
+    $.get(`/api/verify/${id}/${guess}`)
+      .then(() => {
+        charsVerifyResult
+          .text('Correct!')
+          .removeClass('has-text-danger')
+          .addClass('has-text-success');
+        loadStats();
+      })
+      .catch(() => {
+        charsVerifyResult
+          .text('Failed!')
+          .removeClass('has-text-success')
+          .addClass('has-text-danger');
+        loadStats();
+      });
+    verifyCharsButton.attr('disabled', 'true');
+  });
+
+  reloadEquationButton.on('click', loadEquationCaptcha);
+
+  verifyEquationButton.on('click', () => {
+    const id = imgEquation.attr('data-cid');
+    const guess = equationGuess.val();
+    $.get(`/api/verify/${id}/${guess}`)
+      .then(() => {
+        equationVerifyResult
+          .text('Correct!')
+          .removeClass('has-text-danger')
+          .addClass('has-text-success');
+        loadStats();
+      })
+      .catch(() => {
+        equationVerifyResult
+          .text('Failed!')
+          .removeClass('has-text-success')
+          .addClass('has-text-danger');
+        loadStats();
+      });
+    verifyEquationButton.attr('disabled', 'true');
+  });
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
+
+const FC_URL = process.env.FC_URL
 
 const apiRouter = (redisClient) => {
   router.get('/stats', async (req, res) => {
@@ -11,6 +14,42 @@ const apiRouter = (redisClient) => {
       totalVerified: totalVerified || 0,
       totalViewed: totalViewed || 0
     })
+  })
+
+  // Proxy chars captcha creation
+  router.get('/chars', async (req, res) => {
+    const response = await axios.get(`${FC_URL}/api/v1/chars`)
+    res.json({
+      id: response.data.id,
+      url: `${FC_URL}/api/v1/captcha/${response.data.id}`
+    })
+  })
+
+  // Proxy equation captcha creation
+  router.get('/equation', async (req, res) => {
+    const response = await axios.get(`${FC_URL}/api/v1/equation`)
+    res.json({
+      id: response.data.id,
+      url: `${FC_URL}/api/v1/captcha/${response.data.id}`
+    })
+  })
+
+  // Proxy captcha verification
+  router.get('/verify/:id/:guess', (req, res) => {
+    const id = req.params['id']
+    const guess = req.params['guess']
+    axios
+      .get(`${FC_URL}/api/v1/verify/${id}/${guess}`)
+      .then(response => {
+        if (response.status === 200) {
+          res.status(200).end()
+        } else {
+          res.status(400).end()
+        }
+      })
+      .catch(() => {
+        res.status(400).end()
+      })
   })
 
   return router
